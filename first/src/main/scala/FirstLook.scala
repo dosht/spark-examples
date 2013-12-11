9f9c595471ef57f364021fd7c4b21d7c11daab74
Mostafa Sameh <msameh@cloud9ers.com>
2013-12-11 21:47:22 +0200

    code organize

--- first/src/main/scala/FirstLook.scala
+++ first/src/main/scala/FirstLook.scala
@@ -3,21 +3,19 @@ import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
 import org.apache.spark.scheduler.SplitInfo
 
-object SimpleApp {
-  def main(args: Array[String]) {
-    val sparkHome = Option(System getenv "SPARK_HOME") getOrElse "../incubator-spark/"
-    val jarFile = "target/scala-2.10/spark-first_2.10-0.1-SNAPSHOT.jar"
+object SimpleApp extends App {
+  val sparkHome = Option(System getenv "SPARK_HOME") getOrElse "../incubator-spark/"
+  val jarFile = "target/scala-2.10/spark-first_2.10-0.1-SNAPSHOT.jar"
 
-    val logFile = s"$sparkHome/README.md" // Should be some file on your system
-    val sc = new SparkContext("local", "Simple App", sparkHome, List(jarFile), Map(), Map())
+  val logFile = s"$sparkHome/README.md" // Should be some file on your system
+  val sc = new SparkContext("local", "Simple App", sparkHome, List(jarFile), Map(), Map())
 
-    val logData = sc.textFile(logFile, 2).cache()
-    val numAs = logData.filter(line => line.contains("a")).count()
-    val numBs = logData.filter(line => line.contains("b")).count()
-    println(Console.CYAN)
-    println("Lines with a: %s, Lines with b: %s".format(numAs, numBs))
-    println(Console.RESET)
+  val logData = sc.textFile(logFile, 2).cache()
+  val numAs = logData.filter(line => line.contains("a")).count()
+  val numBs = logData.filter(line => line.contains("b")).count()
+  println(Console.CYAN)
+  println("Lines with a: %s, Lines with b: %s".format(numAs, numBs))
+  println(Console.RESET)
 
-    sc.stop()
-  }
+  sc.stop()
 }
