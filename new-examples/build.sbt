e3a91a61930d74a7a7a10b18a724c191bf7098d5
Mostafa Sameh <lambda.mostafa@gmail.com>
2015-10-07 13:33:55 +0100

    PageRank Exmaple

--- new-examples/build.sbt
+++ new-examples/build.sbt
@@ -1,6 +1,6 @@
 name := "Study Examples"
 
-scalaVersion in ThisBuild := "2.11.6"
+scalaVersion in ThisBuild := "2.10.4"
 
 sbtVersion := "0.14.0"
 
@@ -12,7 +12,9 @@ shellPrompt in ThisBuild := { p =>
 val sparkVersion = "1.5.0"
 
 libraryDependencies in ThisBuild ++= Seq(
-  "org.apache.spark" %% "spark-core" % sparkVersion)
+  "org.apache.spark" %% "spark-core" % sparkVersion,
+  "org.apache.spark" %% "spark-graphx" % sparkVersion)
 
 lazy val advancedSpark = project in file("advanced-spark")
 
+lazy val graphx = project in file("graphx")
---
+++ new-examples/graphx/src/main/scala/PageRank.scala
@@ -0,0 +1,47 @@
+import org.apache.spark.{SparkContext, SparkConf}
+import org.apache.log4j.{Level, Logger}
+
+import org.apache.spark.graphx._
+import org.apache.spark.rdd.RDD
+
+import scala.math.Ordering
+
+case class PageWithRank(id: Long, title: String, rank: Double)
+
+object PageWithRank {
+  implicit val pageOrdering = Ordering.by[PageWithRank, Double](_.rank)
+}
+
+object PageRank {
+  def pageHash(title: String): Long = title.##
+
+  def main(args: Array[String]) {
+    Logger.getLogger("org").setLevel(Level.OFF)
+    Logger.getLogger("akka").setLevel(Level.OFF)
+
+    val conf = new SparkConf(false).setMaster("local[*]").setAppName("PageRand-GraphX")
+    val sc = new SparkContext(conf)
+
+    val path = "graphx/links.tsv"
+    val input = sc.textFile(path).filter(!_.startsWith("#")).map(_.split("\t")).cache()
+
+    val pages = input.flatMap(x => x).distinct.map(title => pageHash(title) -> title)
+    val links = input.flatMap {
+      case a@ Array(x, y) => Some(Edge(pageHash(x), pageHash(y), a))
+      case _ => None
+    }
+    val wikiGraph = Graph(pages, links, "")
+
+    val pageRankRDD = wikiGraph.pageRank(tol = 0.0001).vertices.cache() // tol: the tolerance allowed at convergence (smaller => more accurate).
+    val pagesWithRanksRDD: RDD[PageWithRank] = (pages join pageRankRDD) map {
+      case (id, (title, rank)) => PageWithRank(id, title, rank)
+    }
+
+    println("=== Result: ========================")
+    pagesWithRanksRDD.top(10).foreach(println)
+    println("====================================")
+    sc.stop()
+  }
+}
+
+
